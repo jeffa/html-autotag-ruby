9790506ab28c91ca33849af522b940835869094d
jeffa <jeffa@unlocalhost.com>
2015-09-21 14:26:10 -0400

    added constructor

--- lib/HTML/AutoAttr.rb
+++ lib/HTML/AutoAttr.rb
@@ -2,6 +2,17 @@ module HTML
 
     class AutoAttr
 
+        attr_accessor 'hash', 'sorted'
+
+        def initialize( hash = {}, sorted = 0 )
+            @hash   = hash
+            @sorted = sorted
+        end
+
+        def to_s
+
+        end
+
         def key( key )
             key.gsub!( /\s+/, '' )
             key.gsub!( /["'>=\/]/, '' )
