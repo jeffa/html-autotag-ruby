496037587fd843ff2bda159fb2761b889b808b07
jeffa <jeffa@unlocalhost.com>
2015-09-21 20:48:47 -0700

    more tests, structure for tag

--- lib/HTML/AutoTag.rb
+++ lib/HTML/AutoTag.rb
@@ -5,24 +5,37 @@ module HTML
 
   class AutoTag
 
-        attr_accessor 'encodes', 'indent', 'level', 'newline'
+        attr_accessor 'encodes', 'indent', 'level', 'sorted', 'newline'
 
         def initialize( params = {} )
             @encodes    = params['encodes'] || ''
             @indent     = params['indent']  || ''
             @level      = params['level']   || 0
-            @newline    = defined?( params['indent'] ) == nil ? "\n" : ''
+            @sorted     = params['sorted']  || 0
+            @newline    = params['indent'] ? "\n" : ''
 
         end
 
         def tag( params = {} )
-            @tag    = params['encodes']
-            @cdata  = params['indent']
-            @attr   = params['newline']
 
+            attr = HTML::AutoAttr.new( params['attr'], @sorted )
             # emtpy tag
             if defined?( params['cdata'] ) != nil
                 return '<' + params['tag'] + ' />' + @newline
+            end
+
+            if params['cdata'].kind_of?( Array )
+
+                if params['cdata'][0].kind_of?( Hash )
+
+                else
+
+                end
+
+            elsif params['cdata'].kind_of?( Hash )
+
+            else
+
             end
 
         end
