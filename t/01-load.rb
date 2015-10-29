bf3b580db2c3318d06af9c19be7453a87754f838
jeffa <jeffa@unlocalhost.com>
2015-10-29 16:54:37 -0400

    going to roll my own encoder/decoder

--- t/01-load.rb
+++ t/01-load.rb
@@ -5,6 +5,7 @@ class TestLoad < Test::Unit::TestCase
   def test_load
     assert_nothing_raised { require "HTML/AutoTag.rb" }
     assert_nothing_raised { require "HTML/AutoAttr.rb" }
+    assert_nothing_raised { require "HTML/Encoder.rb" }
   end
 
 end
