b57ce94396dbb21ddb552bba957fd8ab544e5352
jeffa <jeffa@unlocalhost.com>
2015-11-04 13:32:30 -0500

    fully implemented HTML::Encoder's functionality

--- t/05-encode.rb
+++ t/05-encode.rb
@@ -58,14 +58,65 @@ class TestHTML_Encoder < Test::Unit::TestCase
 
   end
 
-  def test_container
+  def test_numeric
 
+        encoder = HTML::Encoder.new()
+
+        assert_equal(
+            '&#x26;',
+            encoder.num_entity( '&' ),
+            'char encoded numerically correctly'
+        )
+
+        assert_equal(
+            '&#x26;&#x3C;&#x3E;&#x22;&#x27;',
+            encoder.encode_hex( '&<>"\'' ),
+            'default chars encoded numerically correctly'
+        )
+
+        assert_equal(
+            '&amp;&lt;&gt;&quot;&apos;',
+            encoder.encode( '&<>"\'' ),
+            'does not impact default chars encoded correctly'
+        )
+
+        assert_equal(
+            'hello',
+            encoder.encode_hex( 'hello', '' ),
+            'no chars encoded when no chars requested'
+        )
+
+        assert_equal(
+            'h&#x65;llo',
+            encoder.encode_hex( 'hello', 'e' ),
+            'requested chars encoded correctly'
+        )
+
+        assert_equal(
+            'h&#101;llo',
+            encoder.encode( 'hello', 'e' ),
+            'requested chars encoded correctly'
+        )
 
   end
 
-  def container( params = {} )
+  def test_upper
+
         encoder = HTML::Encoder.new()
-        return encoder.encode( params['rendered'], params['encodes'] ) 
+
+        assert_equal(
+            '&dagger;',
+            encoder.encode( 8224.chr(Encoding::UTF_8), 8224.chr(Encoding::UTF_8) ),
+            'unicode char encoded correctly'
+        )
+
+        str = 9824.chr(Encoding::UTF_8)+9827.chr(Encoding::UTF_8)+9829.chr(Encoding::UTF_8)+9830.chr(Encoding::UTF_8)
+
+        assert_equal(
+            '&spades;&clubs;&hearts;&diams;',
+            encoder.encode( str, str ),
+            'unicode chars encoded correctly'
+        )
   end
 
 end
